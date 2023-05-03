e81df38d4722a814be564fa12b194189d68dfa19
fabricio <fabriciossza@gmail.com>
2023-05-03 11:40:26 -0300

    feat(header): add header and post blueprint

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Post } from './Post.jsx'
+import { Header } from './components/header.jsx'
+import "./styles.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+
+      <Post 
+        author="Blaise Pascal"
+        content="A nossa natureza é o movimento; o completo repouso é a morte."
+      />
+
+      <Post 
+        author="Nikola Tesla"
+        content="Se você quiser descobrir os segredos do Universo, pense em termos de energia, frequência e vibração."
+      />
+    </div>    
   )
 }
-
-export default App
