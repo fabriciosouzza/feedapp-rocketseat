4d1450dd22990831de18fc857818ee6f78bab096
fabricio <fabriciossza@gmail.com>
2023-05-16 17:43:01 -0300

    feat(post): initial props configuration

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,39 @@ import { Post } from './components/Post.jsx'
 import './global.css'
 import styles from './App.module.css'
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1598974357801-cbca100e65d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      name: 'George Boole',
+      role: 'Logician'
+    },
+    content: [ 
+      {type: 'paragraph', content: 'Criador da álgebra booleana, fundamental para o desenvolvimento da computação moderna.'},
+      {type: 'paragraph', content: 'O trabalho de Whitehead e Russell em Principia Mathematica (1910-1913) foi o primeiro a convencer um grupo de matemáticos que a lógica simbólica devia receber sua séria atenção.'},
+      {type: 'link', content: '<a href="https://pt.wikipedia.org/wiki/George_Boole" target= "_blank">👉 Para mais informações</a>'}
+    ],
+    publishedAt: new Date('2023-05-04 10:32:07')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1465153690352-10c1b29577f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80',
+      name: 'Charles Babbage',
+      role: 'Mathematician'
+    },
+    content: [ 
+      {type: 'paragraph', content: 'Charles Babbage é mais conhecido e, de certa forma, referenciado como o inventor que projetou o primeiro computador de uso geral, utilizando apenas partes mecânicas, a máquina analítica.'},
+      {type: 'link', content: '<a href="#">👉 jane.design/doctorcare</a>'}
+    ],
+    publishedAt: new Date('2023-05-05 22:43:15')
+  }
+]
+
+
+
 export function App() {
 
   return (
@@ -14,17 +47,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Blaise Pascal"
-          content="A nossa natureza é o movimento; o completo repouso é a morte."
-          />
-
-          <Post 
-            author="Nikola Tesla"
-            content="Se você quiser descobrir os segredos do Universo, pense em termos de energia, frequência e vibração."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt} 
+              />
+            )
+          })}
         </main>
       </div>
     </div>    
   )
 }
+
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,29 +2,26 @@ import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
-export function Post () {
+export function Post (props) {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
 
-                    <Avatar src="https://images.unsplash.com/photo-1465153690352-10c1b29577f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80" />
+                    <Avatar src={props.author.avatarUrl} />
                     
                     <div className={styles.authorInfo}>
-                        <strong>George Boole</strong>
-                        <span>Logician</span>
+                        <strong>{props.author.name}</strong>
+                        <span>{props.author.role}</span>
                     </div>
 
                 </div>
 
-                <time title='05 de Maio às 09:45h' dateTime="2023-05-11 09:45:17">Publicado há 1h</time>
+                <time title='05 de Maio às 09:45h' dateTime="2023-05-11 09:45:17">{props.publishedAt.toString()}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala Galera👋</p>
-                <p>Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p><a href="#">👉 jane.design/doctorcare</a></p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+               
             </div>
 
             <form className={styles.commentForm}>
